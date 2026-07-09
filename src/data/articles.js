cf6a1649867ad1a2e0f525f6b401133a38849716
oscarcv125 <oscarcv125@users.noreply.github.com>
2026-07-08 21:14:34 -0600

    Add Lanyard component and 3D Ring Builder to portfolio

--- src/data/articles.js
+++
@@ -1,3 +0,0 @@
-const myArticles = [];
-
-export default myArticles;
--- src/data/tracking.js
+++
@@ -1,3 +0,0 @@
-// Google Analytics Tracking ID
-// https://github.com/truethari/reactfolio#-google-analytics
-export const TRACKING_ID = "G-MEASUREMENT_ID"; // Replace with your Google Analytics measurement ID
